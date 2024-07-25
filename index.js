77a351b530e025dbcadd80c3796cbb730f009ebc
moh za <zamoh@orange.fr>
2024-07-25 23:57:04 +0200

    url modified to atlas mango

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017");
+mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
